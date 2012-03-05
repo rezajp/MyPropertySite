9ec0e360be853bb3700a8dee81b9229162e3c01c
Reza Jafari Panah <rezajp@gmail.com>
2012-03-05 11:16:41 +0000

    edit functions

--- MyPropertySite/properysiteapp/views.py
+++ MyPropertySite/properysiteapp/views.py
@@ -19,8 +19,23 @@ def home(request):
     })
     return render_to_response('home.html',variables)
 
-def detail(request,ad_id):
+def ad_detail(request,ad_id):
     ad = get_object_or_404(PropertyAdvertisement, pk=ad_id)
     return render_to_response('ads/detail.html', {'ad': ad})
+def ad_edit(request,ad_id)
+	ad= get_object_or_404(PropertyAdvertisement, pk=ad_id)
+	if(request.method == 'GET')
+		if(isinstance(ad,'RentAdvertisement'))
+			return render_to_response('ads/rent_ad_edit.html',{'ad',ad})
+		elif(isinstance(ad,'SaleAdvertisement'))
+			return render_to_response('ads/sale_ad_edit.html',{'ad',ad})
+	elif(request.method=='POST')
+		if(isinstance(ad,'RentAdvertisement'))
+			#update the add from request
+			return render_to_response('ads/detail.html',{'ad',ad})
+		elif(isinstance(ad,'SaleAdvertisement'))
+			#update the add from request
+			return render_to_response('ads/detail.html',{'ad',ad})
+		
 
 
