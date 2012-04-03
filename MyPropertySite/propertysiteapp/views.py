5566bb84d7242d9bba41e847f4e347691eb0f750
Reza Jafari Panah <rezajp@gmail.com>
2012-04-03 13:50:32 +0100

    add a new rent ad

--- MyPropertySite/propertysiteapp/views.py
+++ MyPropertySite/propertysiteapp/views.py
@@ -28,7 +28,37 @@ def home(request):
 def detail(request,ad_id):
     ad = get_object_or_404(Advertisement, pk=ad_id)
     return render_to_response('ads/detail.html', {'ad': ad})
-
+def new_rent(request):
+	if(request.method=='GET'):
+		return render_to_response('ads/newrent.html')
+	elif(request.method=='POST'):
+		posted = request.POST
+		r= Rent()
+		p= Property()
+		p.address_line1= posted["address_line1"]
+		P.address_line2= posted["address_line2"]
+		p.town=posted["town"]
+		p.country=posted["country"]
+		p.post_code=posted["post_code"]
+		#longitude= models.DecimalField(decimal_places=10,max_digits=20)
+		#latitude =models.DecimalField(decimal_places=10,max_digits=20)
+		p.type_of_property = PropertyType.get(pk=posted["type_of_property"])
+		p.no_of_bedrooms=posted["no_of_bedrooms"]
+		p.no_of_bathrooms=posted["no_of_bathrooms"]
+		#agency = models.ForeignKey('Agency', null=False,related_name='properties')
+		p.save()
+		r.for_property = p
+		r.description= posted["description"]
+		r.slot_start=posted["slot_start"]
+		r.slot_end= posted["slot_end"]
+		#rent.created_at= 
+		r.rent = posted["rent"]
+		r.deposit= posted["deposit"]
+		r.available_on= posted["available_on"]
+		r.save()
+		return render_to_response('ads/detail.html',{'ad',r})
+		
+		
 def ad_edit(request,ad_id):
     ad= get_object_or_404(Advertisement, pk=ad_id)
     if(request.method == 'GET'):
